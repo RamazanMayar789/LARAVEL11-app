00806fc3fbb86a0b03f8391fc29279d133ae94ba
RamazanMayar789 <ghormayar@12345>
2025-01-01 14:22:03 +0430

    Implement admin template assets

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,11 @@
 <?php
 
+use App\Livewire\Admin\Country\Index as CountryIndex;
+use App\Livewire\Admin\Dashboard\Index as DashboardIndex;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get('/', function () {
-    return view('layouts.admin.app');
-});
+Route::get('/admin', DashboardIndex::class)->name('admin.dashboard.index');
+Route::get('/admin/countries',CountryIndex::class)->name('admin.country.index');
 
 
